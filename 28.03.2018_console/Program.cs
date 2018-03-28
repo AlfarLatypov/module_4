ee955a2deed03a7901de6af87483d8bfb5860a6e
ЛатыповА <@223_9>
2018-03-28 19:35:39 +0600

    Add project files.

---
+++ 28.03.2018_console/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28._03._2018_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
